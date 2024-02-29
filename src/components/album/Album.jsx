9064eb7e0916e2e2b90eaf54162aacf17241f238
dimas.asna <dimas.asna@gitlab.com>
2024-02-29 10:48:27 +0700

    last

--- src/components/album/Album.jsx
+++ src/components/album/Album.jsx
@@ -3,6 +3,9 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import posyandu from '../../assets/img/posyandu.jpg'
 import kkn from '../../assets/img/kkn.jpg'
+import senam from '../../assets/img/senam.jpg'
+import pkk from '../../assets/img/pkk.jpg'
+import tani from '../../assets/img/tani.jpg'
 
 import { Autoplay } from 'swiper/modules';
 
@@ -25,31 +28,33 @@ export default function Album() {
                 modules={[Autoplay]}
                 className="mySwiper2">
                 <SwiperSlide>
-                        <div className='w-full h-44 border border-black relative group cursor-pointer'>
+                        <div className='w-full h-44 relative group cursor-pointer shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
                         <img className='absolute w-full' src={kkn} alt="" />
                             <h1 className='absolute p-2 bottom-0 text-white font-bold text-[18px] group-hover:text-[#537561]  '>KKN UAD XV.A.1 2024</h1>
                         </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                        <div className='w-full h-44 border border-black relative group cursor-pointer'>
+                        <div className='w-full h-44 relative group cursor-pointer shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
                             <img className='absolute w-full' src={posyandu} alt="" />
                             <h1 className='absolute p-2 bottom-0  font-bold text-[18px] group-hover:text-[#537561]   text-white'>POSYANDU</h1>
                         </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                        <div className='w-full h-44 border border-black relative group cursor-pointer'>
-                        <img className='absolute w-full' src={kkn} alt="" />
+                        <div className='w-full h-44 relative group cursor-pointer shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+                        <img className='absolute w-full' src={senam} alt="" />
                             <h1 className='absolute p-2 bottom-0 text-white font-bold text-[18px] group-hover:text-[#537561]  '>SENAM</h1>
                         </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                        <div className='w-full h-44 border border-black relative group cursor-pointer'>
-                            <h1 className='absolute p-2 bottom-0  font-bold text-[18px] group-hover:text-[#537561]  '>KEGIATAN PKK</h1>
+                        <div className='w-full h-44 relative group cursor-pointer shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+                        <img className='absolute w-full' src={pkk} alt="" />
+                            <h1 className='absolute p-2 bottom-0 text-white font-bold text-[18px] group-hover:text-[#537561]  '>KEGIATAN PKK</h1>
                         </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                        <div className='w-full h-44 border border-black relative group cursor-pointer'>
-                            <h1 className='absolute p-2 bottom-0  font-bold text-[18px] group-hover:text-[#537561]  '>BERCOCOK TANAM</h1>
+                        <div className='w-full h-44 relative group cursor-pointer shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
+                        <img className='absolute w-full' src={tani} alt="" />
+                            <h1 className='absolute p-2 bottom-0 text-white font-bold text-[18px] group-hover:text-[#537561]  '>BERCOCOK TANAM</h1>
                         </div>
                 </SwiperSlide>
                 
